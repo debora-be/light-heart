3e61b1872e52d98124795bb957685b04cfa6c828
debora berte <arobedbee@gmail.com>
2024-03-30 15:18:04 -0300

    infra: create nix files

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A basic flake to run an Gleam Project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (pkgs.lib) optional optionals;
+        pkgs = import nixpkgs { inherit system; };
+
+        inputs = with pkgs;
+          [ gleam glibcLocales ]
+          ++ optional stdenv.isLinux [ inotify-tools ]
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin
+          (with darwin.apple_sdk.frameworks; [ CoreFoundation CoreServices ]);
+      in with pkgs; {
+        devShells.default = mkShell {
+          name = "Light Heart";
+          packages = inputs;
+        };
+      });
+}
